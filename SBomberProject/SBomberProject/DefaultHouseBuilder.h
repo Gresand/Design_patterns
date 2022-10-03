de91a7887af83d584ea78ca1cc41d634637c32de
Андрей Грес <andreygress@gmail.com>
2022-10-03 20:45:26 +0300

    Builder and Bridge

---
+++ SBomberProject/SBomberProject/DefaultHouseBuilder.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "HouseBuilder.h"
+
+class DefaultHouseBuilder : public HouseBuilder
+{
+	void createHouse() override;
+	void buildMakeDefaultRoof() override;
+	void buildMakeDefaultWall() override;
+	void buildMakeDefaultWindow() override;
+	HouseBlocks* getBuitHouse() override
+	{
+		return pHouseBlocks;
+	}
+};
+
