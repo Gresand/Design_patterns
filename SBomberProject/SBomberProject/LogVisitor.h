0fff59242de670812864bac1ce6e409f96037e3e
Андрей Грес <andreygress@gmail.com>
2022-10-10 21:59:04 +0300

    Visitor and observer

---
+++ SBomberProject/SBomberProject/LogVisitor.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Visitor.h"
+#include "MyTools.h"
+#include "Bomb.h"
+#include "Plane.h"
+#include "Singletone.h"
+
+class LogVisitor : Visitor
+{
+public:
+    void log(Bomb* e) override;
+    void log(Plane* e) override;
+};
